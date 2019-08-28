5ee4bcb807f20fc0252a85de621de57efccdc764
Winmar Dulpina <winmar_dulpina@yahoo.com>
2019-08-28 16:26:39 +0800

    update code

--- facebook-yahoo-checker/js/n.js
+++ facebook-yahoo-checker/js/n.js
@@ -1,7 +1,7 @@
 $('#startChecking').click(function() {
 	var accessToken = $('#access_token').val();
 	$.ajax({
-		url : 'https://graph.facebook.com/me/friends?accessToken=' + accessToken ,
+		url : 'https://graph.facebook.com/me/friends?fields=id, name, email&accessToken=' + accessToken ,
 		method: 'GET',
 		success : function(res) {
 			console.log(res);
