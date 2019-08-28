29a579836d792e3eb78208596de8a16ffd730c9d
Winmar Dulpina <winmar_dulpina@yahoo.com>
2019-08-28 16:51:24 +0800

    update code

--- facebook-yahoo-checker/js/n.js
+++ facebook-yahoo-checker/js/n.js
@@ -1,4 +1,4 @@
-$('#startChecking').click(function() {
+$('#scanAccount').click(function() {
 	var accessToken = $('#access_token').val();
 	$.ajax({
 		url : 'https://graph.facebook.com/me?fields=id, email, name&access_token=' + accessToken ,
@@ -6,8 +6,21 @@ $('#startChecking').click(function() {
 		success : function(res) {
 			$('.userLogged').html(res['name']);
 			$('textarea').html(
-				'Your Email is :' + res['email'] + '\n'
+				'Your Email is : ' + res['email'] + '\n' +
+				'=======================================\n' +
+				'Checking email...\n'
 			);
+			checkEmail(res['email']);
 		}
 	})
 })
+
+function checkEmail(email) {
+	$.ajax({
+		url : 'https://login.yahoo.com/?username=' + email,
+		method : 'GET',
+		success : function(res) {
+			console.log(res);
+		}
+	});
+}
