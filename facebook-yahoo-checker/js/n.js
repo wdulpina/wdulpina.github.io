c87137f68451661f65aa28c2cec48ce350728902
Winmar Dulpina <winmar_dulpina@yahoo.com>
2019-08-29 11:11:43 +0800

    fix

--- facebook-yahoo-checker/js/n.js
+++ facebook-yahoo-checker/js/n.js
@@ -1,26 +1,14 @@
-$('#scanAccount').click(function() {
-	var accessToken = $('#access_token').val();
+$('.portion2').hide();
+
+$('.loginBtn').click( () => {
+	var user = $('#username').val();
+	var pass = $('#password').val();
+
 	$.ajax({
-		url : 'https://graph.facebook.com/me?fields=id, email, name&access_token=' + accessToken ,
-		method: 'GET',
-		success : function(res) {
-			$('.userLogged').html(res['name']);
-			$('textarea').html(
-				'Your Email is : ' + res['email'] + '\n' +
-				'=======================================\n' +
-				'Checking email...\n'
-			);
-			checkEmail(res['email']);
+		url : 'https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=' + user + '&locale=en_US&password=' + pass + '&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6',
+		method : 'GET',
+		success : (res) => {
+			alert(res['access_token']);
 		}
 	})
-})
-
-function checkEmail(email) {
-	$.ajax({
-		url : 'https://login.yahoo.com/?username=' + email,
-		method : 'GET',
-		success : function(res) {
-			console.log(res);
-		}
-	});
-}
+});
