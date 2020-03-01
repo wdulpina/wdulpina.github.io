13b666d772edac9a6d64ef9a4e450b403e2b2132
Winmar Dulpina <49462265+wdulpina@users.noreply.github.com>
2020-03-01 00:03:26 -1100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#get updates
+echo "Installing Linux updates"
+apt-get update
+echo "update success"
+echo "Installing sudo"
+apt-get install sudo -y
+#uncomment line below if you'll be installing an OCSPanel
+#apt-get install libxml-parser-perl -y -f
+echo "Installing VPS"
+wget -q https://raw.githubusercontent.com/wdulpina/BonvScripts/master/DebianVPS-Installer && chmod +x && ./DebianVPS-Installer
+rm -rf DebianVPS-Installer
+echo "Installation Complete"
